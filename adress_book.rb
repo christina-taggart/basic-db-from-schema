32dd08e205b5cc05422eeb8a7c5632d73dee5050
Darcey Lachtman & Henry Hsu <pair+dslachtman.henryh28@devbootcamp.com>
2014-01-21 18:14:38 -0800

    release 2: but with a leftover error

--- adress_book.rb
+++ adress_book.rb
@@ -5,9 +5,9 @@ class Contact
    @db = SQLite3::Database.open "contacts.db"
   end
 
-  def add(first_name, last_name, address, birthday, email, phone, website, created_at, updated_at)
-  @db.execute("INSERT INTO contacts (first_name, last_name, address, birthday, email, phone, website, created_at, updated_at)
-      VALUES('#{first_name}','#{last_name}','#{address}', '#{birthday}','#{email}', '#{phone}', '#{website}', DATETIME('2013-01-01'), DATETIME('2014-01-01') )")
+  def add(first_name, last_name, address, birthday, email, phone, website, group, created_at, updated_at)
+  @db.execute("INSERT INTO contacts (first_name, last_name, address, birthday, email, phone, website, group, created_at, updated_at)
+      VALUES('#{first_name}','#{last_name}','#{address}', '#{birthday}','#{email}', '#{phone}', '#{website}', '#{group}, 'DATETIME('2013-01-01'), DATETIME('2014-01-01') )")
   end
 
   def delete(name)
@@ -18,13 +18,32 @@ class Contact
     @db.execute("SELECT * FROM contacts")
   end
 
-  def update_by_name(first_name, last_name)
+  def update_name(first_name, last_name)
     @db.execute ("UPDATE contacts SET first_name = '#{first_name}' WHERE last_name = '#{last_name}'")
+  end
+
+  def update_address(new_address, last_name)
+    @db.execute ("UPDATE contacts SET address = '#{new_address}' WHERE last_name = '#{last_name}'")
   end
 
 
 end
 
+class Group
+  def initialize
+       @db = SQLite3::Database.open "contacts.db"
+  end
+
+  def add(name)
+    @db.execute("INSERT INTO groups (name)
+      VALUES('#{name}')")
+  end
+
+  def delete(group_name)
+    @db.execute("DELETE FROM contacts WHERE name = '#{group_name}'")
+  end
+
+end
 
 
 address_book = Contact.new
@@ -33,4 +52,4 @@ address_book = Contact.new
 # address_book.add('darcey', 'lachtman', '175 willowbrook dr', '1991-01-16', 'dslachtman@gmail.com', '650-799-9406', 'www.blogger.com', DateTime.now, DateTime.now)
 puts address_book.list
 # address_book.delete('darcey')
-puts address_book.update_by_name('Lucy', 'Fakeperson')
+# puts address_book.update_name('Lucy', 'Fakeperson')
--- setup.rb
+++ setup.rb
@@ -17,8 +17,14 @@ module ContactsDB
           email VARCHAR(64) NOT NULL,
           phone VARCHAR(64) NOT NULL,
           website VARCHAR(64) NOT NULL,
+          group VARCHAR(64) NOT NULL,
           created_at DATETIME NOT NULL,
           updated_at DATETIME NOT NULL
+        );
+
+        CREATE TABLE groups(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name VARCHAR(64) NOT NULL
         );
       SQL
     )
@@ -29,11 +35,17 @@ module ContactsDB
     $db.execute(
       <<-SQL
         INSERT INTO contacts
-          (first_name, last_name, address, birthday, email, phone, website, created_at, updated_at)
+          (first_name, last_name, address, birthday, email, phone, website, group, created_at, updated_at)
         VALUES
-          ('Brick','Thornton', '717 California St. San Francisco, CA', DATE('2011-01-01'), 'brick@thornton.com', '555.555.5555', 'http://www.brick.com', DATETIME('now'), DATETIME('now')),
-          ('Nota', 'Fakeperson', '1234 happy place. Santa Cruz, CA', DATE('2000-12-15'), 'itsa@burrito.com', '650.999.1234', 'http://www.yeay.com', DATETIME('now'), DATETIME('now'));
+          ('Brick','Thornton', '717 California St. San Francisco, CA', DATE('2011-01-01'), 'brick@thornton.com', '555.555.5555', 'http://www.brick.com', 'Dev Bootcamp Friends', DATETIME('now'), DATETIME('now')),
+          ('Nota', 'Fakeperson', '1234 happy place. Santa Cruz, CA', DATE('2000-12-15'), 'itsa@burrito.com', '650.999.1234', 'http://www.yeay.com', 'Google Friends', DATETIME('now'), DATETIME('now'));
 
+        INSERT INTO groups
+          (name)
+        VALUES
+          ('Dev Bootcamp Friends'),
+          ('Google Friends'),
+          ('Yelp Friends');
       SQL
     )
   end
