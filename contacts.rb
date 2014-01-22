8bd22b290c4e34689f37d68bef15205a3ae9f9a3
Christina Taggart & <pair+christina-taggart.hzla00@devbootcamp.com>
2014-01-21 17:40:35 -0800

    Release 1: Built Contacts and Groups classes

---
+++ contacts.rb
@@ -0,0 +1,52 @@
+require 'sqlite3'
+
+class Contact
+
+  attr_reader :first_name, :last_name, :email, :phone, :created_at, :updated_at
+  def initialize(data = {})
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @email = data[:email]
+    @phone = data[:phone]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+  end
+
+  def add(database)
+    database.execute(
+      <<-SQL
+        INSERT INTO contacts
+          (first_name, last_name, email, phone, created_at, updated_at)
+        VALUES
+          ('#{first_name}','#{last_name}', '#{email}', '#{phone}', '#{created_at}', '#{updated_at}');
+        SQL
+    )
+  end
+
+  def delete(database)
+    database.execute("delete from contacts where first_name = '#{first_name}' and last_name = '#{last_name}'")
+  end
+
+   def self.update_record(database, attribute, value, id)
+
+    database.execute("update contacts set #{attribute} = '#{value}' where id = #{id}")
+  end
+
+end
+
+
+class Group
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+address_book = SQLite3::Database.new "address_book.db"
+andy = Contact.new(first_name: "Andy", last_name: "Lee", email: "email", phone: "123-456-2313",
+  created_at: Time.now, updated_at: Time.now)
+#andy.add(address_book)
+#andy.delete(address_book)
+Contact.update_record(address_book, "email", 'aleebc.edu', 5)
