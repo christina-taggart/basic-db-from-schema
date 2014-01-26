9f6701329406f672b62005936aaa0ab8317194bc
K Y <kevin@kevinyeh.com>
2014-01-25 23:19:57 -0800

    Add create group and create contact methods

--- address_book.rb
+++ address_book.rb
@@ -27,6 +27,50 @@ module AddressBook
 			)
 	end
 
+	def self.add_contact(contact)
+		$db.execute(
+			<<-SQL
+			INSERT INTO contacts
+			(first_name, last_name, company, phone_number, email, created_at, updated_at)
+			values
+			('#{contact.first_name}', '#{contact.last_name}', '#{contact.company}', 
+				'#{contact.phone_number}', '#{contact.email}', DATETIME('now'), DATETIME('now'));
+			SQL
+			)
+	end
+
+
+	def self.add_group(group)
+		$db.execute(
+			<<-SQL
+			INSERT INTO groups
+			(name, created_at, updated_at)
+			values
+			('#{group.name}', DATETIME('now'), DATETIME('now'));
+			SQL
+			)
+	end
+end
+
+class Contact
+	attr_accessor :first_name, :last_name, :company, :phone_number, :email
+
+	def initialize(params = {})
+		@first_name = params[:first_name]
+		@last_name = params[:last_name]
+		@company = params[:company]
+		@phone_number = params[:phone_number]
+		@email = params[:email]
+	end
+
+end
+
+class Group
+	attr_accessor :name
+
+	def initialize(params = {})
+		@name = params[:name]
+	end
 end
 
 
@@ -37,3 +81,17 @@ AddressBookDB::seed
 p AddressBook::select_all_contacts
 p AddressBook::select_all_groups
 p AddressBook::select_all_contacts_groups
+
+# Release 2: Add a contact
+
+contact = Contact.new(first_name: "Kevin", last_name: "Federline",
+											company: "addidas", phone_number: "123-123-1234",
+											email: "kfed@email.com")
+
+AddressBook::add_contact(contact)
+p AddressBook::select_all_contacts
+
+group = Group.new(name: 'exes')
+
+AddressBook::add_group(group)
+p AddressBook::select_all_groups
